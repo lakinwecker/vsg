84cb0b8aee223cc42205893c4c5a454266a45e9f
Hesam Damghanian <32716448+Sk7w4tch3r@users.noreply.github.com>
2023-04-02 21:57:45 -0600

    Doctest to 2.4.10

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class Recipe(ConanFile):
     def requirements(self):
         requirements = [
             "immer/0.8.0",
-            "doctest/2.4.9",
+            "doctest/2.4.10",
         ]
         for r in requirements:
             self.requires(r)
